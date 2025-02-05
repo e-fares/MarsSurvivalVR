6e2e97ed96797863d21b726788065714a45f238b
ELIE FARES - SD46424 <elie.fares@stellantis.com>
2025-02-05 17:48:07 +0100

    Clean Project

---
+++ Assets/Scripts/ConsoleToTextMesh.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class ConsoleToTextMesh: MonoBehaviour
+{
+    public TextMeshProUGUI consoleText;
+    private string logContent = "";
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        logContent += logString + "\n";
+        if (logContent.Length > 3000) // Limite la longueur pour éviter trop de texte
+        {
+            logContent = logContent.Substring(logContent.Length - 3000);
+        }
+        consoleText.text = logContent;
+    }
+}
